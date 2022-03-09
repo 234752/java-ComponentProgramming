ce6cf53ab1e3e87e3eca8845439340e437e78279
Patryk <patryk.gielcz@gmail.com>
2022-03-09 16:12:50 +0100

    checkstyle partly fixed

---
+++ 00_FirstJava/src/main/java/pl/first/firstjava/Sudoku.java
@@ -0,0 +1,10 @@
+package pl.first.firstjava;
+
+public class Sudoku {
+
+    public static void main(String[] args) {
+        SudokuBoard sb = new SudokuBoard();
+        sb.fillBoard();
+        sb.print();
+    }
+}
--- 00_FirstJava/src/main/java/pl/first/firstjava/SudokuBoard.java
+++ 00_FirstJava/src/main/java/pl/first/firstjava/SudokuBoard.java
@@ -1,68 +1,42 @@
 package pl.first.firstjava;
+
 import java.util.Random;
 import java.util.Vector;
 
-public class SudokuBoard
-{
+public class SudokuBoard {
   Random random = new Random();
-    private int[][] board = new int[9][9];
+  private int[][] board = new int[9][9];
 
-//    public int getRandomInt(int[] v)
-//    {
-//        while(true)
-//        {
-//
-//            int index = random.nextInt(8);
-//            //System.out.println(index);
-//            if(v[index] != 0)
-//            {
-//                int n = v[index];
-//                v[index] = 0;
-//                return n;
-//            }
-//        }
-//    }
 
-    public boolean isValid(int row, int col, int n)
-    {
-        for (int i = 0; i < 9; i++)
-        {
-            if (board[row][i] == n) { return false; }                                                                   //check columns
+    public boolean isValid(int row, int col, int n) {
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == n) { return false; }                                               //check columns
         }
 
-        for (int i = 0; i < 9; i++)
-        {
-            if (board[i][col] == n) { return false; }                                                                   //check rows
+        for (int i = 0; i < 9; i++) {
+            if (board[i][col] == n) { return false; }                                               //check rows
         }
 
         int boxRowStart = row - row % 3;
         int boxColStart = col - col % 3;
 
-        for (int iR = boxRowStart; iR < boxRowStart + 3; iR++)
-        {
-            for (int iC = boxColStart; iC < boxColStart + 3; iC++)
-            {
-                if (board[iR][iC] == n)
-                {
-                    return false;                                                                                       //check squares
+        for (int iR = boxRowStart; iR < boxRowStart + 3; iR++) {
+            for (int iC = boxColStart; iC < boxColStart + 3; iC++) {
+                if (board[iR][iC] == n) {
+                    return false;                                                                   //check squares
                 }
             }
         }
-        return true;                                                                                                    //if all 3 are OK
+        return true;                                                                                //if all 3 are OK
     }
 
-    private boolean solveBoard()
-    {
-
+    private boolean solveBoard() {
         int row = -1;
         int col = -1;
         boolean isEmpty = true;
-        for (int iR = 0; iR < 9; iR++)
-        {
-            for (int iC = 0; iC < 9; iC++)
-            {
-                if (board[iR][iC] == 0)
-                {
+        for (int iR = 0; iR < 9; iR++) {
+            for (int iC = 0; iC < 9; iC++) {
+                if (board[iR][iC] == 0) {
                     row = iR;
                     col = iC;
                     isEmpty = false;
@@ -70,80 +44,39 @@ public class SudokuBoard
                 }
             }if (!isEmpty) { break; }
         }
-        if (isEmpty) { return true; }                                                                                   //end with all fields filled
+        if (isEmpty) { return true; }                                                                //end with all fields filled
 
 
-        for (int num = 1; num <= 9; num++)
-        {
-            if (isValid(row, col, num))
-            {
+        for (int num = 1; num <= 9; num++) {
+            if (isValid(row, col, num)) {
                 board[row][col] = num;
-                if (solveBoard())
-                {
+                if (solveBoard()) {
                     return true;
                 }
-                else
-                {
+                else {
                     board[row][col] = 0;
                 }
             }
         }
-
-//        for (int i = 0; i < 9; i++)
-//        {
-//            int[] values = new int[] {1,2,3,4,5,6,7,8,9};
-//            int n = getRandomInt(values);
-//            //System.out.println(i+" "+n);
-//            if (isValid(row, col, n))
-//            {
-//                board[row][col] = n;
-//                if (fillBoard())
-//                {
-//                    return true;
-//                }
-//                else
-//                {
-//                    board[row][col] = 0;
-//                }
-//            }
-//        }
         return false;
     }
 
-    public void print()
-    {
-        for (int iR = 0; iR < 9; iR++)
-        {
-            for (int iC = 0; iC < 9; iC++)
-            {
+    public void print() {
+        for (int iR = 0; iR < 9; iR++) {
+            for (int iC = 0; iC < 9; iC++) {
                 System.out.print(board[iR][iC]);
                 System.out.print(" ");
             }
             System.out.print("\n");
         }
     }
-    public void fillBoard()
-    {
-        for(int i=1; i<=9; i++)
-        {
+    public void fillBoard() {
+        for(int i=1; i<=9; i++) {
             board[random.nextInt(9)][random.nextInt(9)] = i;
         }
 
         this.solveBoard();
     }
-    public static void main(String[] args)
-    {
-        SudokuBoard sb = new SudokuBoard();
 
-        for(int i=0; i<10; i++)
-        {
-            sb.fillBoard();
-
-            sb.print();
-            System.out.println("-----");
-        }
-
-
-    }
 
 }
