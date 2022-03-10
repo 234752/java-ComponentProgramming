09839ff770e34f1359ac2cfdf454ebcf428ec8ec
Patryk <patryk.gielcz@gmail.com>
2022-03-10 15:54:28 +0100

    indents

--- 00_FirstJava/src/main/java/pl/first/firstjava/SudokuBoard.java
+++ 00_FirstJava/src/main/java/pl/first/firstjava/SudokuBoard.java
@@ -11,75 +11,75 @@ public class SudokuBoard {
       return board[row][col];
   }
 
-    private boolean isValid(int row, int col, int n) {
-        for (int i = 0; i < 9; i++) {
-            if (board[row][i] == n) {
-                return false;
-            }
-        }
+  private boolean isValid(int row, int col, int n) {
+      for (int i = 0; i < 9; i++) {
+          if (board[row][i] == n) {
+              return false;
+          }
+      }
 
-        for (int i = 0; i < 9; i++) {
-            if (board[i][col] == n) {
-                return false;
-            }
-        }
+      for (int i = 0; i < 9; i++) {
+          if (board[i][col] == n) {
+              return false;
+          }
+      }
 
-        int boxRowStart = row - row % 3;
-        int boxColStart = col - col % 3;
+      int boxRowStart = row - row % 3;
+      int boxColStart = col - col % 3;
 
-        for (int rowIterator = boxRowStart; rowIterator < boxRowStart + 3; rowIterator++) {
-            for (int colIterator = boxColStart; colIterator < boxColStart + 3; colIterator++) {
-                if (board[rowIterator][colIterator] == n) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
+      for (int rowIterator = boxRowStart; rowIterator < boxRowStart + 3; rowIterator++) {
+          for (int colIterator = boxColStart; colIterator < boxColStart + 3; colIterator++) {
+              if (board[rowIterator][colIterator] == n) {
+                  return false;
+              }
+          }
+      }
+      return true;
+  }
 
-    private boolean solveBoard() {
-        int row = -1;
-        int col = -1;
-        boolean isEmpty = true;
-        for (int rowIterator = 0; rowIterator < 9; rowIterator++) {
-            for (int colIterator = 0; colIterator < 9; colIterator++) {
-                if (board[rowIterator][colIterator] == 0) {
-                    row = rowIterator;
-                    col = colIterator;
-                    isEmpty = false;
-                    break;
-                }
-            }
-            if (!isEmpty) {
-                break;
-            }
-        }
-        if (isEmpty) {
-            return true;
-        }
+  private boolean solveBoard() {
+      int row = -1;
+      int col = -1;
+      boolean isEmpty = true;
+      for (int rowIterator = 0; rowIterator < 9; rowIterator++) {
+          for (int colIterator = 0; colIterator < 9; colIterator++) {
+              if (board[rowIterator][colIterator] == 0) {
+                  row = rowIterator;
+                  col = colIterator;
+                  isEmpty = false;
+                  break;
+              }
+          }
+          if (!isEmpty) {
+              break;
+          }
+      }
+      if (isEmpty) {
+          return true;
+      }
 
-        for (int num = 1; num <= 9; num++) {
-            if (isValid(row, col, num)) {
-                board[row][col] = num;
-                if (solveBoard()) {
-                    return true;
-                } else {
-                    board[row][col] = 0;
-                }
-            }
-        }
-        return false;
-    }
+      for (int num = 1; num <= 9; num++) {
+          if (isValid(row, col, num)) {
+              board[row][col] = num;
+              if (solveBoard()) {
+                  return true;
+              } else {
+                  board[row][col] = 0;
+              }
+          }
+      }
+      return false;
+  }
 
-    public void print() {
-        for (int rowIterator = 0; rowIterator < 9; rowIterator++) {
-            for (int colIterator = 0; colIterator < 9; colIterator++) {
-                System.out.print(board[rowIterator][colIterator]);
-                System.out.print(" ");
-            }
-            System.out.print("\n");
-        }
-    }
+  public void print() {
+      for (int rowIterator = 0; rowIterator < 9; rowIterator++) {
+          for (int colIterator = 0; colIterator < 9; colIterator++) {
+              System.out.print(board[rowIterator][colIterator]);
+              System.out.print(" ");
+          }
+          System.out.print("\n");
+      }
+  }
 
   public void fillBoard() {
       for (int row = 0; row < 9; row++) {
