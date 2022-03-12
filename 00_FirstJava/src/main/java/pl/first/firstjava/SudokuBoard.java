08bb58cef69796ea436c6347bc30e6efa36ad274
Patryk <patryk.gielcz@gmail.com>
2022-03-12 18:04:51 +0100

    comments

--- 00_FirstJava/src/main/java/pl/first/firstjava/SudokuBoard.java
+++ 00_FirstJava/src/main/java/pl/first/firstjava/SudokuBoard.java
@@ -13,13 +13,13 @@ public class SudokuBoard {
 
   private boolean isValid(int row, int col, int n) {
       for (int i = 0; i < 9; i++) {
-          if (board[row][i] == n) {
+          if (board[row][i] == n) {                             //check for repetition in row
               return false;
           }
       }
 
       for (int i = 0; i < 9; i++) {
-          if (board[i][col] == n) {
+          if (board[i][col] == n) {                             //check for repetition in column
               return false;
           }
       }
@@ -29,7 +29,7 @@ public class SudokuBoard {
 
       for (int rowIterator = boxRowStart; rowIterator < boxRowStart + 3; rowIterator++) {
           for (int colIterator = boxColStart; colIterator < boxColStart + 3; colIterator++) {
-              if (board[rowIterator][colIterator] == n) {
+              if (board[rowIterator][colIterator] == n) {       //check for repetition in 3x3 box/square
                   return false;
               }
           }
@@ -43,10 +43,10 @@ public class SudokuBoard {
       boolean isEmpty = true;
       for (int rowIterator = 0; rowIterator < 9; rowIterator++) {
           for (int colIterator = 0; colIterator < 9; colIterator++) {
-              if (board[rowIterator][colIterator] == 0) {
+              if (board[rowIterator][colIterator] == 0) {       //looking for field where value is 0 (not filled yet)
                   row = rowIterator;
                   col = colIterator;
-                  isEmpty = false;
+                  isEmpty = false;                              //double break in case of finding it
                   break;
               }
           }
@@ -55,23 +55,23 @@ public class SudokuBoard {
           }
       }
       if (isEmpty) {
-          return true;
+          return true;                                          //board is filled, return from recursion
       }
 
       for (int num = 1; num <= 9; num++) {
-          if (isValid(row, col, num)) {
+          if (isValid(row, col, num)) {                         //check if any of numbers 1-9 can fit current field
               board[row][col] = num;
-              if (solveBoard()) {
+              if (solveBoard()) {                               //recursively check if this insertion leads to solution
                   return true;
               } else {
                   board[row][col] = 0;
               }
           }
       }
-      return false;
+      return false;                                             //if none of the values make board solvable - backtrack
   }
 
-  public void print() {
+  public void print() {                                         //output method
       for (int rowIterator = 0; rowIterator < 9; rowIterator++) {
           for (int colIterator = 0; colIterator < 9; colIterator++) {
               System.out.print(board[rowIterator][colIterator]);
@@ -82,16 +82,16 @@ public class SudokuBoard {
   }
 
   public void fillBoard() {
-      for (int row = 0; row < 9; row++) {
+      for (int row = 0; row < 9; row++) {                       //fill with 0s
           for (int col = 0; col < 9; col++) {
               board[row][col] = 0;
           }
       }
 
-      for (int i = 1; i <= 9; i++) {
+      for (int i = 1; i <= 9; i++) {                            //fill 9 random fields with digits 1-9
           board[random.nextInt(9)][random.nextInt(9)] = i;
       }
-      this.solveBoard();
+      this.solveBoard();                                       //fill remaining fields according to sudoku rules
     }
 
 }
