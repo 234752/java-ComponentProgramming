0012eb4fcf66ce1da9647dc2fb3160c9c24eedf6
Patryk <patryk.gielcz@gmail.com>
2022-03-15 15:58:08 +0100

    import order

--- Sudoku/src/main/java/SudokuBoard.java
+++ Sudoku/src/main/java/SudokuBoard.java
@@ -1,8 +1,7 @@
-import java.util.Collections;
-import java.util.Random;
-import java.util.List;
 import java.util.Arrays;
-
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
 
 public class SudokuBoard {
 
