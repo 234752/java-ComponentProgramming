a5ce51b2d9e4ffd998de23d8dd498f2f6ef224a2
Patryk <patryk.gielcz@gmail.com>
2022-03-16 15:29:41 +0100

    names pt. 2

--- Sudoku/src/main/java/SudokuBoard.java
+++ Sudoku/src/main/java/SudokuBoard.java
@@ -4,16 +4,15 @@ public class SudokuBoard {
     private int[][] internalBoard = new int[9][9];
     private SudokuSolver sudokuSolver = new BacktrackingSudokuSolver();
 
-    public int get(int row, int col) {
-        return internalBoard[row][col];
+    public int get(int x, int y) {
+        return internalBoard[x][y];
     }
 
-    public void set(int row, int col, int value){
-        internalBoard[row][col] = value;
+    public void set(int x, int y, int value){
+        internalBoard[x][y] = value;
     }
 
     public void solveGame() {
-
         sudokuSolver.solve(this);                                       //fill remaining fields according to sudoku rules
     }
 }
