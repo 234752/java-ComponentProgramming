f5c684e89dfd93169b545063dacd8c2f7f8b76be
Patryk <patryk.gielcz@gmail.com>
2022-03-16 15:25:57 +0100

    adding 0s removed

--- Sudoku/src/main/java/SudokuBoard.java
+++ Sudoku/src/main/java/SudokuBoard.java
@@ -1,10 +1,8 @@
-import java.util.Random;
 
 public class SudokuBoard {
 
-    Random random = new Random();
     private int[][] internalBoard = new int[9][9];
-    private SudokuSolver solver = new BacktrackingSudokuSolver();
+    private SudokuSolver sudokuSolver = new BacktrackingSudokuSolver();
 
     public int get(int row, int col) {
         return internalBoard[row][col];
@@ -14,82 +12,8 @@ public class SudokuBoard {
         internalBoard[row][col] = value;
     }
 
-//    private boolean isValid(SudokuBoard board, int row, int col, int n) {
-//        for (int i = 0; i < 9; i++) {
-//            if (board[row][i] == n) {                             //check for repetition in row
-//                return false;
-//            }
-//        }
-//
-//        for (int i = 0; i < 9; i++) {
-//            if (board[i][col] == n) {                             //check for repetition in column
-//                return false;
-//            }
-//        }
-//
-//        int boxRowStart = row - row % 3;
-//        int boxColStart = col - col % 3;
-//
-//        for (int rowIterator = boxRowStart; rowIterator < boxRowStart + 3; rowIterator++) {
-//            for (int colIterator = boxColStart; colIterator < boxColStart + 3; colIterator++) {
-//                if (board[rowIterator][colIterator] == n) {       //check for repetition in 3x3 box/square
-//                    return false;
-//                }
-//            }
-//        }
-//        return true;
-//    }
-
-//    private boolean solveBoard(SudokuBoard board) {
-//        int row = -1;
-//        int col = -1;
-//        boolean isEmpty = true;
-//        for (int rowIterator = 0; rowIterator < 9; rowIterator++) {
-//            for (int colIterator = 0; colIterator < 9; colIterator++) {
-//                if (board[rowIterator][colIterator] == 0) {       //looking for field where value is 0 (not filled yet)
-//                    row = rowIterator;
-//                    col = colIterator;
-//                    isEmpty = false;                              //double break in case of finding it
-//                    break;
-//                }
-//            }
-//            if (!isEmpty) {
-//                break;
-//            }
-//        }
-//        if (isEmpty) {
-//            return true;                                          //board is filled, return from recursion
-//        }
-//
-//        Integer[] values = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-//        List<Integer> valList = Arrays.asList(values);
-//        Collections.shuffle(valList);
-//        valList.toArray(values);
-//
-//        for (int num = 0; num < 9; num++) {
-//            if (isValid(row, col, values[num])) {                         //check if any of numbers 1-9 can fit current field
-//                board[row][col] = values[num];
-//                if (solveBoard()) {                               //recursively check if this insertion leads to solution
-//                    return true;
-//                } else {
-//                    board[row][col] = 0;
-//                }
-//            }
-//        }
-//        return false;                                             //if none of the values make board solvable - backtrack
-//    }
-
     public void solveGame() {
-        for (int row = 0; row < 9; row++) {                       //fill with 0s
-            for (int col = 0; col < 9; col++) {
-                internalBoard[row][col] = 0;
-            }
-        }
 
-        for (int i = 1; i <= 9; i++) {                            //fill 9 random fields with digits 1-9
-            internalBoard[random.nextInt(9)][random.nextInt(9)] = i;
-        }
-        solver.solve(this);                                       //fill remaining fields according to sudoku rules
+        sudokuSolver.solve(this);                                       //fill remaining fields according to sudoku rules
     }
-
 }
