0806f553c76751efb6b56b7b78e6a552e36b3829
Patryk <patryk.gielcz@gmail.com>
2022-03-21 18:20:44 +0100

    sudokuField class

---
+++ Sudoku/src/main/java/SudokuField.java
@@ -0,0 +1,12 @@
+public class SudokuField {
+
+    private int value;
+
+    public int getFieldValue() {
+        return value;
+    }
+
+    public void setFieldValue(int newValue) {
+        value = newValue;
+    }
+}
