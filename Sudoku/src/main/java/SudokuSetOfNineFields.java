d3625a691b99bdb9132431645cbe6a7dcde711aa
Patryk <patryk.gielcz@gmail.com>
2022-03-22 14:25:01 +0100

    verify implemented

--- Sudoku/src/main/java/SudokuSetOfNineFields.java
+++ Sudoku/src/main/java/SudokuSetOfNineFields.java
@@ -1,8 +1,17 @@
 public class SudokuSetOfNineFields {
 
-    private SudokuField fields = new SudokuField();
+    private SudokuField[] fields = new SudokuField[9];
 
     public boolean verify() {
+
+        int[] values = new int[10];
+
+        for (int i=0; i<9; i++) {
+            values[fields[i].getFieldValue()]++;
+        } //count amount of appearances of each digit
+        for (int v=1; v<=9; v++) {
+            if (values[v] >= 2) return false;
+        } //check if any digit repeats
         return true;
     }
 }
