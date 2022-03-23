37afe8d852fb3e804b44f68931f3be74fb9481b8
Patryk <patryk.gielcz@gmail.com>
2022-03-23 19:43:16 +0100

    test for observer

---
+++ Sudoku/src/test/java/ObserverTest.java
@@ -0,0 +1,19 @@
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class ObserverTest {
+
+    public ObserverTest() {
+    }
+    SudokuBoard testedBoard = new SudokuBoard(new BacktrackingSudokuSolver());
+
+    @Test
+    public void testIfObserverActivates() {
+        testedBoard.set(8,8,1);
+        testedBoard.set(7,7,1);//error message should NOT be printed
+        testedBoard.addObserver(new Observer());
+        testedBoard.set(7,7,0);
+        testedBoard.set(7,7,1); //error message should be printed
+    }
+}
