7159e4d1187f20799d45a23a0d58bea051ee0ca5
Patryk <patryk.gielcz@gmail.com>
2022-03-31 12:18:12 +0200

    changed array to list in sudoku solver

--- Sudoku/src/main/java/BacktrackingSudokuSolver.java
+++ Sudoku/src/main/java/BacktrackingSudokuSolver.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -26,13 +27,18 @@ public class BacktrackingSudokuSolver implements SudokuSolver {
             return true;                                          //board is filled, return from recursion
         }
 
-        Integer[] values = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-        List<Integer> valList = Arrays.asList(values);
-        Collections.shuffle(valList);
-        valList.toArray(values);
+        //Integer[] values = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        //List<Integer> valList = Arrays.asList(values);
+        //Collections.shuffle(valList);
+        //valList.toArray(values);
+        List<Integer> values = new ArrayList<>();
+        for (int i = 1; i <= 9; i++) {
+            values.add(i);
+        }
+        Collections.shuffle(values);
 
         for (int num = 0; num < 9; num++) {
-            board.set(row, col, values[num]);
+            board.set(row, col, values.get(num));
             if (board.isValid(row, col) && solve(board)) {        //check if any of numbers 1-9 can fit current field
                 return true;                                      //and if it leads to solution (recursively)
             } else {
