0b98fa0ef552002ceef31da59878118923ea0aa7
Patryk <patryk.gielcz@gmail.com>
2022-03-31 12:19:37 +0200

    removed import

--- Sudoku/src/main/java/BacktrackingSudokuSolver.java
+++ Sudoku/src/main/java/BacktrackingSudokuSolver.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -27,10 +26,6 @@ public class BacktrackingSudokuSolver implements SudokuSolver {
             return true;                                          //board is filled, return from recursion
         }
 
-        //Integer[] values = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-        //List<Integer> valList = Arrays.asList(values);
-        //Collections.shuffle(valList);
-        //valList.toArray(values);
         List<Integer> values = new ArrayList<>();
         for (int i = 1; i <= 9; i++) {
             values.add(i);
