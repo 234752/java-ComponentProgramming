0a7b2df647700ac18ed14638d8dc64b0e51a8a2c
Patryk <patryk.gielcz@gmail.com>
2022-04-13 19:29:36 +0200

    new class

---
+++ Sudoku/Dao/src/main/java/FileSudokuBoardDao.java
@@ -0,0 +1,15 @@
+import solver.BacktrackingSudokuSolver;
+
+public class FileSudokuBoardDao<T> implements Dao, AutoCloseable{
+
+
+    @Override
+    public Object read() {
+        return null;
+    }
+
+    @Override
+    public void write(Object obj) {
+
+    }
+}
