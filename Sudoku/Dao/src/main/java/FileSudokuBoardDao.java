837add17890fbeaff07f67f5f4e9588e826e6f62
Patryk <patryk.gielcz@gmail.com>
2022-04-14 19:53:34 +0200

    read and write to file

--- Sudoku/Dao/src/main/java/FileSudokuBoardDao.java
+++ Sudoku/Dao/src/main/java/FileSudokuBoardDao.java
@@ -1,3 +1,7 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
 public class FileSudokuBoardDao<SudokuBoard> implements Dao<SudokuBoard>, AutoCloseable {
 
     private String filename;
@@ -8,12 +12,17 @@ public class FileSudokuBoardDao<SudokuBoard> implements Dao<SudokuBoard>, AutoCl
 
     @Override
     public SudokuBoard read() {
-        return null;
+        FileInputStream input = new FileInputStream(filename);
+        SudokuBoard board = (SudokuBoard)input.read();
+        input.close();
+        return board;
     }
 
     @Override
     public void write(SudokuBoard obj) {
-
+        FileOutputStream output = new FileOutputStream(filename);
+        output.write(obj);
+        output.close();
     }
 
     @Override
