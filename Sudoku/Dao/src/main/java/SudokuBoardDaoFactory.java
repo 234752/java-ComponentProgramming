d05d9d64ed87340579c3bb984d1ab998dda49d5c
Patryk <patryk.gielcz@gmail.com>
2022-04-28 13:26:03 +0200

    private constructor

--- Sudoku/Dao/src/main/java/SudokuBoardDaoFactory.java
+++ Sudoku/Dao/src/main/java/SudokuBoardDaoFactory.java
@@ -2,6 +2,8 @@ import model.SudokuBoard;
 
 public class SudokuBoardDaoFactory {
 
+    private SudokuBoardDaoFactory() {}
+
     public static Dao<SudokuBoard> getFileDao(String filename) {
         return new FileSudokuBoardDao<>(filename);
     }
