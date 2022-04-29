4da4d7cf41ab999095b19e6eb34b93521127539d
Patryk <patryk.gielcz@gmail.com>
2022-04-29 20:04:54 +0200

    test of compareTo

---
+++ Sudoku/Model/src/test/java/SudokuFieldTest.java
@@ -0,0 +1,30 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import model.SudokuBoard;
+import model.SudokuField;
+
+import org.junit.jupiter.api.Test;
+import solver.BacktrackingSudokuSolver;
+
+public class SudokuFieldTest {
+
+    @Test
+    public void testCompareFields() {
+        SudokuBoard testedBoard = new SudokuBoard(new BacktrackingSudokuSolver());
+        SudokuField testedField1 = new SudokuField(testedBoard);
+        SudokuField testedField2 = new SudokuField(testedBoard);
+
+        testedField1.setFieldValue(7);
+        testedField2.setFieldValue(7);
+        assertEquals(testedField1.compareTo(testedField2), 0);
+
+        testedField1.setFieldValue(1);
+        testedField2.setFieldValue(9);
+        assertEquals(testedField1.compareTo(testedField2), -8);
+
+        testedField1.setFieldValue(6);
+        testedField2.setFieldValue(3);
+        assertEquals(testedField1.compareTo(testedField2), 3);
+
+    }
+}
