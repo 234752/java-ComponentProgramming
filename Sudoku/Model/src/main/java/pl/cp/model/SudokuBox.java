974d43da14e9138d717282194a9f2a2239c5851e
Patryk <patryk.gielcz@gmail.com>
2022-05-01 21:26:00 +0200

    deep copy of SudokuBox

--- Sudoku/Model/src/main/java/pl/cp/model/SudokuBox.java
+++ Sudoku/Model/src/main/java/pl/cp/model/SudokuBox.java
@@ -1,4 +1,18 @@
 package pl.cp.model;
 
 public class SudokuBox extends SudokuSetOfNineFields {
+
+    public void set(int x, int y, int value) {
+        int index = 3 * x + y;
+        super.set(index, value);
+    }
+
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        SudokuBox box = new SudokuBox();
+        for (int i = 0; i < this.fields.size(); i++) {
+            box.addNextField((SudokuField) this.fields.get(i).clone());
+        }
+        return box;
+    }
 }
