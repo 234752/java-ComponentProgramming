70cc3a0f717231aeb7dbf88e8c6e8c11189ab6d7
Patryk <patryk.gielcz@gmail.com>
2022-05-03 15:41:54 +0200

    sample board is shown when app starts

--- Sudoku/GUI/src/main/java/pl/cp/gui/SudokuApplication.java
+++ Sudoku/GUI/src/main/java/pl/cp/gui/SudokuApplication.java
@@ -6,7 +6,10 @@ import javafx.collections.FXCollections;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+import pl.cp.solver.BacktrackingSudokuSolver;
 import javafx.stage.Stage;
+import pl.cp.model.SudokuBoard;
 
 public class SudokuApplication extends Application {
     @Override
@@ -18,6 +21,19 @@ public class SudokuApplication extends Application {
         stage.show();
         ChoiceBox pog = (ChoiceBox) scene.lookup("#pog");
         pog.setItems(FXCollections.observableArrayList("Small Pog","Medium Pog","Big Pog"));
+        TextField fields[][] = new TextField[9][9];
+        for (int x = 0; x < 9; x++) {
+            for (int y = 0; y < 9; y++) {
+                fields[x][y] = (TextField) scene.lookup("#tf"+Integer.toString(x)+Integer.toString(y));
+            }
+        }
+        SudokuBoard mainBoard = new SudokuBoard(new BacktrackingSudokuSolver());
+        mainBoard.solveGame();
+        for (int x = 0; x < 9; x++) {
+            for (int y = 0; y < 9; y++) {
+                fields[x][y].setText(Integer.toString(mainBoard.get(x, y)));
+            }
+        }
     }
 
     public static void main(String[] args) {
