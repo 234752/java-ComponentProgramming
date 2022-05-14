d1746524af1a216493bf294f01070a726eb62b7a
Patryk <patryk.gielcz@gmail.com>
2022-05-14 11:28:43 +0200

    authors list resource bundle classes

---
+++ Sudoku/GUI/src/main/java/pl/cp/author/Author_en.java
@@ -0,0 +1,13 @@
+package pl.cp.author;
+
+import java.util.ListResourceBundle;
+
+public class Author_en extends ListResourceBundle {
+
+    @Override
+    protected Object[][] getContents() {
+        return new Object[][]{
+                {"a1", "Patrick Gielczynski"}
+        };
+    }
+}
---
+++ Sudoku/GUI/src/main/java/pl/cp/author/Author_pl.java
@@ -0,0 +1,13 @@
+package pl.cp.author;
+
+import java.util.ListResourceBundle;
+
+public class Author_pl extends ListResourceBundle {
+
+    @Override
+    protected Object[][] getContents() {
+        return new Object[][] {
+                {"a1", "Patryk Giełczyński"}
+        };
+    }
+}
