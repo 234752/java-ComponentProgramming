9b331ee242ffd2c97d27dff4dc3a245bb927683e
Patryk <patryk.gielcz@gmail.com>
2022-05-30 21:58:33 +0200

    prototype of board insert

--- Sudoku/Model/src/main/java/pl/cp/dao/JDBCDao.java
+++ Sudoku/Model/src/main/java/pl/cp/dao/JDBCDao.java
@@ -1,11 +1,8 @@
 package pl.cp.dao;
 
 import pl.cp.exception.DaoException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+
+import java.sql.*;
 
 public class JDBCDao<SudokuBoard> implements Dao<SudokuBoard> {
 
@@ -16,12 +13,54 @@ public class JDBCDao<SudokuBoard> implements Dao<SudokuBoard> {
             JDBCDao db = new JDBCDao();
 
             db.connect();
-            db.DDL();
+            db.makeBoard();
         }
 
         public void connect() throws SQLException {
             String dbUrl = "jdbc:derby:memory:SudokuDB;create=true";
             conn = DriverManager.getConnection(dbUrl);
+        }
+
+        public void makeBoard() throws SQLException{
+
+            Statement statement = conn.createStatement();
+
+
+            // create table
+            //statement.executeUpdate("Create table boards (id int primary key)");
+            String pog = "Create table boards (id int primary key";
+
+            for (int i = 0; i<3; i++) {
+                for (int j = 0; j<3; j++) {
+                    pog += ", f"+i+j+" int";
+                }
+            }
+
+            pog+=")";
+
+            PreparedStatement ps = conn.prepareStatement(pog);
+            ps.execute();
+
+            // insert 2 rows
+            statement.executeUpdate("insert into boards values (102,1,2,3,4,5,6,7,8,9)");
+
+            // query
+            ResultSet set = statement.executeQuery("SELECT * FROM boards");
+
+            // print out query result
+            while (set.next()) {
+                System.out.printf("%d\t\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n", set.getInt("id"),
+                        set.getInt("f00"),
+                        set.getInt("f01"),
+                        set.getInt("f02"),
+                        set.getInt("f10"),
+                        set.getInt("f11"),
+                        set.getInt("f12"),
+                        set.getInt("f20"),
+                        set.getInt("f21"),
+                        set.getInt("f22")
+                );
+            }
         }
 
         public void DDL() throws SQLException {
