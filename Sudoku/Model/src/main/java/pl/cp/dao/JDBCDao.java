2c5cd0601582690b38284205d25e2d9fd248c4a5
Patryk <patryk.gielcz@gmail.com>
2022-05-31 20:22:24 +0200

    new method - empty board creation in database

--- Sudoku/Model/src/main/java/pl/cp/dao/JDBCDao.java
+++ Sudoku/Model/src/main/java/pl/cp/dao/JDBCDao.java
@@ -7,91 +7,71 @@ import java.sql.*;
 public class JDBCDao<SudokuBoard> implements Dao<SudokuBoard> {
 
 
-        Connection conn;
+    Connection conn;
 
-        public static void main(String[] args) throws SQLException {
-            JDBCDao db = new JDBCDao();
+    public static void main(String[] args) throws SQLException {
+        JDBCDao db = new JDBCDao();
 
-            db.connect();
-            db.makeBoard();
-        }
+        db.connect();
+        db.createNewBoard(102);
+    }
 
-        public void connect() throws SQLException {
-            String dbUrl = "jdbc:derby:memory:SudokuDB;create=true";
-            conn = DriverManager.getConnection(dbUrl);
-        }
+    public void connect() throws SQLException {
+        String dbUrl = "jdbc:derby:memory:SudokuDB;create=true";
+        conn = DriverManager.getConnection(dbUrl);
+    }
 
-        public void makeBoard() throws SQLException{
+    public void createNewBoard(int id) throws SQLException{
 
-            Statement statement = conn.createStatement();
+        Statement statement = conn.createStatement();
 
-            String pog = "Create table boards (id int primary key";
+        String ddl = "Create table boards (id int primary key";
 
-            for (int i = 0; i<3; i++) {
-                for (int j = 0; j<3; j++) {
-                    pog += ", f"+i+j+" int";
-                }
-            }
-
-            pog+=")";
-
-            PreparedStatement ps = conn.prepareStatement(pog);
-            ps.execute();
-
-            statement.executeUpdate("insert into boards (id) values (102)");
-            for (int i = 0; i<3; i++) {
-                for (int j = 0; j<3; j++) {
-                    String sql = "update boards set f"+i+j+" = " +j+ " where id = 102";
-                    PreparedStatement ps2 = conn.prepareStatement(sql);
-                    ps2.execute();
-                }
-            }
-
-            // query
-            ResultSet set = statement.executeQuery("SELECT * FROM boards");
-
-            // print out query result
-            while (set.next()) {
-                System.out.printf("%d\t\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n", set.getInt("id"),
-                        set.getInt("f00"),
-                        set.getInt("f01"),
-                        set.getInt("f02"),
-                        set.getInt("f10"),
-                        set.getInt("f11"),
-                        set.getInt("f12"),
-                        set.getInt("f20"),
-                        set.getInt("f21"),
-                        set.getInt("f22")
-                );
+        for (int i = 0; i<9; i++) {
+            for (int j = 0; j<9; j++) {
+                ddl += ", f"+i+j+" int";
             }
         }
+        ddl+=")";
 
-        public void DDL() throws SQLException {
-            Statement statement = conn.createStatement();
+        statement.executeUpdate(ddl);
 
-            // drop table
-            // stmt.executeUpdate("Drop Table users");
+        PreparedStatement initialInsert = conn.prepareStatement("insert into boards (id) values (?)");
+        initialInsert.setObject(1, id);
+        initialInsert.execute();
 
-            // create table
-            statement.executeUpdate("Create table boards (id int primary key, f00 int, f01 int, f02 int)");
+        //statement.executeUpdate("insert into boards (id) values (102)");
+        //for (int i = 0; i<3; i++) {
+        //    for (int j = 0; j<3; j++) {
+        //        String sql = "update boards set f"+i+j+" = " +j+ " where id = 102";
+        //        PreparedStatement ps2 = conn.prepareStatement(sql);
+        //        ps2.execute();
+        //    }
+        //}
 
-            // insert 2 rows
-            statement.executeUpdate("insert into boards values (1,1,2,3)");
-            statement.executeUpdate("insert into boards values (2,4,5,6)");
 
+        /*
+        // query
+        ResultSet set = statement.executeQuery("SELECT * FROM boards");
 
-            // query
-            ResultSet set = statement.executeQuery("SELECT * FROM boards");
-
-            // print out query result
-            while (set.next()) {
-                System.out.printf("%d\t%s\t%s\t%s\n", set.getInt("id"),
-                        set.getInt("f00"),
-                        set.getInt("f01"),
-                        set.getInt("f02")
-                );
-            }
+        // print out query result
+        while (set.next()) {
+           System.out.printf("%d\t\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n", set.getInt("id"),
+                    set.getInt("f00"),
+                    set.getInt("f01"),
+                    set.getInt("f02"),
+                    set.getInt("f10"),
+                    set.getInt("f11"),
+                    set.getInt("f12"),
+                    set.getInt("f20"),
+                    set.getInt("f21"),
+                    set.getInt("f22")
+            );
         }
+
+         */
+    }
+
 
     @Override
     public SudokuBoard read() throws DaoException {
