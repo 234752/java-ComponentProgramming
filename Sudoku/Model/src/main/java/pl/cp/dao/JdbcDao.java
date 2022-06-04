614ac2ac95550aa9909fe0b244c00b5b31e0d41c
Patryk <patryk.gielcz@gmail.com>
2022-06-04 12:23:33 +0200

    PMD

--- Sudoku/Model/src/main/java/pl/cp/dao/JdbcDao.java
+++ Sudoku/Model/src/main/java/pl/cp/dao/JdbcDao.java
@@ -7,7 +7,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ResourceBundle;
-
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import pl.cp.exception.DaoException;
